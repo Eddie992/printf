c2a19a66e528e1c0a959a259267c3d7e3127720b
Edward kataika <ekataika2002@gmail.com>
2022-05-18 21:39:52 +0200

    intial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Contributers
+Edward Kataika
+Michael Mwanza
---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAINH
+#define MAINH
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+#endif
